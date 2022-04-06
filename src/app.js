003e7ca1e307e9c93f66240b234c39f66227a327
KozaCarry <30726826+KozaCarry@users.noreply.github.com>
2022-04-06 14:01:06 +0300

    Initial project version

---
+++ src/app.js
@@ -0,0 +1,15 @@
+document.body.onload = addElement;
+//var my_div = (newDiv = null);
+
+function addElement() {
+  // Создаём новый элемент div
+  // и добавляем в него немного контента
+
+  var newDiv = document.createElement("div");
+  newDiv.innerHTML = "<h1>Привет!</h1>";
+
+  // Добавляем только что созданный элемент в дерево DOM
+
+  my_div = document.getElementById("org_div1");
+  document.body.insertBefore(newDiv, my_div);
+}
